e782fb55117aab4499bd6c4bd2be5a7616e86285
Officina Execute <officina-execute@example.com>
2026-07-30 22:42:12 +0000

    execute: card-dfcc62f296b44574

--- demo_officina/__init__.py
+++ demo_officina/__init__.py
@@ -2,6 +2,7 @@
 
 from demo_officina.saludar import saludar as saludar
 from demo_officina.despedir import despedir as despedir
+from demo_officina.gritar import gritar as gritar
 
 __version__ = "0.1.0"
 
