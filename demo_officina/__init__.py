59391263175c2df46ed0ef218281a1bb2e156e7c
Officina Execute <officina-execute@example.com>
2026-07-30 21:29:17 +0000

    execute: card-fbe6abeda47a488f

--- demo_officina/__init__.py
+++ demo_officina/__init__.py
@@ -3,6 +3,12 @@
 from demo_officina.saludar import saludar as saludar
 from demo_officina.despedir import despedir as despedir
 
+
+def invertir(texto: str) -> str:
+    """Regresa la cadena de texto invertida."""
+    return texto[::-1]
+
+
 __version__ = "0.1.0"
 
 
