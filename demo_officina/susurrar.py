3d1fe1a8717fefac4eef4d95090261920fa82928
Officina Execute <officina-execute@example.com>
2026-08-03 18:33:31 +0000

    execute: card-1088e913c1dc

--- demo_officina/susurrar.py
+++ demo_officina/susurrar.py
@@ -4,3 +4,8 @@
 def susurrar(texto: str) -> str:
     """Regresa el texto en minúsculas."""
     return texto.lower()
+
+
+def susurrar_dos_veces(texto: str) -> str:
+    """Regresa el texto en minúsculas repetido dos veces."""
+    return texto.lower() * 2
